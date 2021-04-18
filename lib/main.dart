a553e6d910f27c34116b4dc7cad2e2470c181974
Wellington Costa <fcowellcosta@gmail.com>
2021-04-18 15:22:44 -0300

    Create one Create one

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(MaterialApp
+  (
+    /*Título é opcional*/
+      title:"Contador de Pessoas",
+      /*Criando um width para criação da tela*/
+      home: Container(color: Colors.white,)
+  )
+);
+
+}
