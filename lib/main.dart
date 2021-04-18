25a89004165d926bfbd3fa832f202a36b39e03b3
Wellington Costa <fcowellcosta@gmail.com>
2021-04-18 15:56:54 -0300

    Inserting columns and texts in the home

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,22 @@ runApp(MaterialApp
     /*Título é opcional*/
       title:"Contador de Pessoas",
       /*Criando um width para criação da tela*/
-      home: Container(color: Colors.white,)
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: <Widget>[
+          Text("Pessoas:0",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold),
+          ),
+          Text("Pode Entrar",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,fontSize: 30.0),
+          )
+        ]
+      ) 
   )
 );
 
